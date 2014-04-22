3ffb8f0512d2c49da78b1ca522c0be1faa8e0801
xtzeng <xiaoti_zeng@126.com>
2014-04-22 14:04:31 +0800

    add comment on ArrayCopy

--- src/test/java/com/xt/basic/ArrayCopyTest.java
+++ src/test/java/com/xt/basic/ArrayCopyTest.java
@@ -1,5 +1,7 @@
 package com.xt.basic;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class ArrayCopyTest {
@@ -17,7 +19,13 @@ public class ArrayCopyTest {
         //输出数组dest 
         for(int i=0;i<dest.length;i++) { 
             System.out.println(dest[i]); 
-        } 
+        }
+        System.out.println(Arrays.toString(dest));
+      //arraycopy(Object src, int srcStartIndex, Object dest, int destStartIndex, int length) 
+      //从指定源数组中复制一个数组，复制从指定的位置开始，到目标数组的指定位置结束。 
+      //srcStartIndex：原数组中要开始复制的第一个元素的位置 
+      //destStartIndex: 目标数组中要开始替换的第一个元素的位置 
+      //length: 要复制的元素的个数
 	}
 	
 	
